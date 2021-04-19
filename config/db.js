f0bdf447ec087a4330756ab848c97dd729c013d9
rene <renebitter@gmail.com>
2021-04-19 13:35:38 -0300

    Connect MongoDB

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// const config = require('config');
+// const db = config.get('mongoURI');
+
+require('dotenv').config();
+const db = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
