b4f0e2ff8f2f75b34a71765aa22a1f87aae49d4c
rene <renebitter@gmail.com>
2021-04-19 12:45:10 -0300

    First commit
    Initial server and routes with http methods

---
+++ routes/contacts.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+// CRUD ROUTE
+
+// @route   GET api/contacts
+// @desc    Get all users contacts
+// @access  Private
+router.get('/', (req, res) => {
+  res.send('Get all contacts');
+});
+
+// @route   POST api/contacts
+// @desc    Add new contac
+// @access  Private
+router.post('/', (req, res) => {
+  res.send('Add contact');
+});
+
+// @route   PUT api/contacts/:id
+// @desc    Update contac
+// @access  Private
+router.put('/:id', (req, res) => {
+  res.send('Update contact');
+});
+
+// @route   PUT api/contacts/:id
+// @desc    Delete contac
+// @access  Private
+router.delete('/:id', (req, res) => {
+  res.send('Delete contact');
+});
+
+module.exports = router;
