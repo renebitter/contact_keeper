a75e8d1a3cda51d392a05e96ca0eab515b60f96d
rene <renebitter@gmail.com>
2021-04-21 09:37:52 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 // @desc    Register a user
 // @access  Public
 router.post('/', (req, res) => {
-  res.send('Register a user');
+  res.send(req.body);
 });
 
 module.exports = router;
