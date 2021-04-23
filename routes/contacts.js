8343ef19ecc905b5f063874a6a5198013afbbfae
rene <renebitter@gmail.com>
2021-04-22 22:39:11 -0300

    Add Contact Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
-
 const User = require('../models/User');
 const Contact = require('../models/Contact');
 
@@ -24,21 +23,49 @@ router.get('/', auth, async (req, res) => {
 });
 
 // @route   POST api/contacts
-// @desc    Add new contac
+// @desc    Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required').not().isEmpty()]],
+  // auth,
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route   PUT api/contacts/:id
-// @desc    Update contac
+// @desc    Update contact
 // @access  Private
 router.put('/:id', (req, res) => {
   res.send('Update contact');
 });
 
 // @route   PUT api/contacts/:id
-// @desc    Delete contac
+// @desc    Delete contact
 // @access  Private
 router.delete('/:id', (req, res) => {
   res.send('Delete contact');
