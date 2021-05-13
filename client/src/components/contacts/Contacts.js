9013e5c39c4e9bbb6679eb995eaa91a5e88f4ff5
rene <renebitter@gmail.com>
2021-05-13 14:50:05 -0300

    Get & Clear Contacts
    Spinner

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,25 +1,38 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import ContactItem from './ContactItem';
+import Spinner from '../layout/Spinner';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts, filtered } = contactContext;
+  const { contacts, filtered, getContacts, loading } = contactContext;
 
-  if (contacts.length === 0) {
+  useEffect(() => {
+    getContacts();
+    // eslint-disable-next-line
+  }, []);
+
+  if (contacts !== null && contacts.length === 0 && !loading) {
     return <h4>Please add a contact</h4>;
   }
+  console.log(contacts);
 
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map((contact) => (
-            <ContactItem key={contact._id} contact={contact} />
-          ))
-        : contacts.map((contact) => (
-            <ContactItem key={contact._id} contact={contact} />
-          ))}
+      {contacts !== null && !loading ? (
+        <>
+          {filtered !== null
+            ? filtered.map((contact) => (
+                <ContactItem key={contact._id} contact={contact} />
+              ))
+            : contacts.map((contact) => (
+                <ContactItem key={contact._id} contact={contact} />
+              ))}
+        </>
+      ) : (
+        <Spinner />
+      )}
     </Fragment>
   );
 };
---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Spinner = () => {
+  return (
+    <Fragment>
+      <div className='spinner'></div>
+    </Fragment>
+  );
+};
+
+export default Spinner;
